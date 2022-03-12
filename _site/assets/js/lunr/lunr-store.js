2b368f74889ea9ea34cb416bd732029f9e51d555
Aman-Agrawal01 <amanag2019@gmail.com>
2022-03-13 00:07:17 +0530

    Added Dark Theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,85 @@
+var store = [{
+        "title": "Portfolio item number 1",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-1/",
+        "teaser": null
+      },{
+        "title": "Portfolio item number 2",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-2/",
+        "teaser": null
+      },{
+        "title": "Hello World",
+        "excerpt":"My first blog! Stay tuned for more.  ","categories": [],
+        "tags": ["cool posts"],
+        "url": "/posts/2021/10/hello-world/",
+        "teaser": null
+      },{
+        "title": " Quantum Query Complexity",
+        "excerpt":"My first official blog! I have been learning query complexity and thought of writing it in a blog. This blog requires some basic understanding of quantum computing specifically the postulates of quantum mechanics. Query Model In Query Model, our task is to compute the function $f(x_1,x_2,….,x_n)$ by accessing $x_i$ through...","categories": [],
+        "tags": ["quantum","complexity"],
+        "url": "/posts/2021/11/query_complexity/",
+        "teaser": null
+      },{
+        "title": "Generative Adversarial Networks",
+        "excerpt":"Generative Adversarial Networks or in short GAN’s have the most interesting and appealing applications in my opinion ranging from generating new samples for data augmentation to create deepfakes or transforming images from one domain to the other domain. So, I thought of writing about this and appreciate the math behind...","categories": [],
+        "tags": ["ML"],
+        "url": "/posts/2022/2/gans/",
+        "teaser": null
+      },{
+        "title": "Paper Title Number 1",
+        "excerpt":"This paper is about the number 1. The number 2 is left for future work.   Download paper here   Recommended citation: Your Name, You. (2009). “Paper Title Number 1.” Journal 1. 1(1).  ","categories": [],
+        "tags": [],
+        "url": "/publication/2009-10-01-paper-title-number-1",
+        "teaser": null
+      },{
+        "title": "Paper Title Number 2",
+        "excerpt":"This paper is about the number 2. The number 3 is left for future work.   Download paper here   Recommended citation: Your Name, You. (2010). “Paper Title Number 2.” Journal 1. 1(2).  ","categories": [],
+        "tags": [],
+        "url": "/publication/2010-10-01-paper-title-number-2",
+        "teaser": null
+      },{
+        "title": "Paper Title Number 3",
+        "excerpt":"This paper is about the number 3. The number 4 is left for future work.   Download paper here   Recommended citation: Your Name, You. (2015). “Paper Title Number 3.” Journal 1. 1(3).  ","categories": [],
+        "tags": [],
+        "url": "/publication/2015-10-01-paper-title-number-3",
+        "teaser": null
+      },{
+        "title": "Talk 1 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "/talks/2012-03-01-talk-1",
+        "teaser": null
+      },{
+        "title": "Tutorial 1 on Relevant Topic in Your Field",
+        "excerpt":"More information here   This is a description of your tutorial, note the different field in type. This is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "/talks/2013-03-01-tutorial-1",
+        "teaser": null
+      },{
+        "title": "Talk 2 on Relevant Topic in Your Field",
+        "excerpt":"More information here   This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "/talks/2014-02-01-talk-2",
+        "teaser": null
+      },{
+        "title": "Conference Proceeding talk 3 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your conference proceedings talk, note the different field in type. You can put anything in this field.  ","categories": [],
+        "tags": [],
+        "url": "/talks/2014-03-01-talk-3",
+        "teaser": null
+      },{
+        "title": "Teaching experience 1",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post.   Heading 1   Heading 2   Heading 3  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2014-spring-teaching-1",
+        "teaser": null
+      },{
+        "title": "Teaching experience 2",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post.   Heading 1   Heading 2   Heading 3  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2015-spring-teaching-1",
+        "teaser": null
+      }]
